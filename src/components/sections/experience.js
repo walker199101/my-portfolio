8ef4192e89e09bf565be2ca2db901f5305ea32b8
g-kim <walker199101@gmail.com>
2022-04-25 11:12:41 +0900

    경력란 내용 수정

--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -3,7 +3,6 @@ import './experience.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAt, faGraduationCap, faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
-
 function Experience() {
     return (
         <div className="container">
@@ -27,16 +26,15 @@ function Experience() {
                     <div className="list-block">
                         <div className="date">2017 ~ 2019 (2년)</div>
                         <h3><span className="timeline-icon"><FontAwesomeIcon icon={faBriefcase} /></span>프론트엔드 개발자 <span className="at-icon"><FontAwesomeIcon icon={faAt} />KeyPort Solutions</span></h3>
-                        <p>웹사이트 및 하이브리드 모바일 어플리케이션 개발 (Vuejs, HTML, CSS)</p>
-                        <p>웹사이트 및 하이브리드 모바일 어플리케이션 개발 (Vuejs, HTML, CSS)</p>
-                        <p>웹사이트 및 하이브리드 모바일 어플리케이션 개발 (Vuejs, HTML, CSS)</p>
+                        <p>미쓰비시 UFJ 증권 구좌 개설 모바일 페이지 개발 (VueJs)</p>
+                        <p>E-커머스 웹사이트 Organic Story 개발 (CSS, HTML)</p>
+                        <p>모바일 페이지용 하이브리드 어플리케이션 개발 (VueJs, Cordova)</p>
                     </div>
                     <div className="list-block">
                         <div className="date">2019 ~ 2021 (1년 10개월)</div>
                         <h3><span className="timeline-icon"><FontAwesomeIcon icon={faBriefcase} /></span>프론트엔드 개발자 <span className="at-icon"><FontAwesomeIcon icon={faAt} />씨에스이</span></h3>
-                        <p>NTT 데이터센터 웹사이트 화면단 개발 (React)</p>
-                        <p>NTT 데이터센터 웹사이트 화면단 개발 (React)</p>
-                        <p>NTT 데이터센터 웹사이트 화면단 개발 (React)</p>
+                        <p>NTT 데이터센터 모니터링 웹사이트 화면단 개발 (React)</p>
+                        <p>모바일용 웹페이지 화면 개발 및 UI 개선 작업 (React)</p>
                     </div>
                     <span className="line"></span>
                 </div>
